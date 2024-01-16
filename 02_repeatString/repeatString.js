51ecf349b4594703de35c646f58e3c9cd38a1623
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-01-15 22:30:24 -0500

    Complete ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    var text = '';
+    if(num<0){
+        return "ERROR"
+    } else {
+        for (let i = num; i > 0; i--) {
+            text += string;
+        }
+        return text;
+    }
 };
 
 // Do not edit below this line
