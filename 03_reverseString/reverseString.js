bcd4c77035ee72257d93c982740ef06b5958cd42
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-01-15 22:47:17 -0500

    Pass ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str = '';
+    for(let i = string.length; i > 0; i--) {
+        str += string.slice(-1);
+        string = string.slice(0,-1);
+    }
+    return str;
 };
 
 // Do not edit below this line
