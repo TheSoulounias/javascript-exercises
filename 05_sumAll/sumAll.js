212c5d4ab4918f31b86bb7bdf36cfb83c8665192
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-02-13 22:50:35 -0500

    Try first draft

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
 
+    let theSum = 0;
+    for(let newX=x;newX<y;newX++) {
+        theSum += newX;
+    }
+    theSum += y;
+    return theSum;
 };
 
 // Do not edit below this line
