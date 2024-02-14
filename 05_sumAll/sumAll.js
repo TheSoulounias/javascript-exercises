a4fdcc30f3ff185cd117e1a08d1e51a26db3dcd2
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-02-13 23:02:57 -0500

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,24 @@
-const sumAll = function(x,y) {
+const sumAll = function (x, y) {
 
     let theSum = 0;
-    for(let newX=x;newX<y;newX++) {
-        theSum += newX;
+    if (x < 0 || y < 0 || !Number.isInteger(x)|| !Number.isInteger(y)) {
+        return "ERROR";
     }
-    theSum += y;
-    return theSum;
+    else if (x < y) {
+        for (let newX = x; newX < y; newX++) {
+            theSum += newX;
+        }
+        theSum += y;
+        return theSum;
+    }
+    else if (y < x) {
+        for (let newY = y; newY < x; newY++) {
+            theSum += newY;
+        }
+        theSum += x;
+        return theSum;
+    }
+
 };
 
 // Do not edit below this line
