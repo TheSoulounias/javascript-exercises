c08910ad8e0bc3f6d9e6dc8a2f1fabd5307c3e25
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-03-17 16:10:53 -0400

    finnished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (Number(num) < 0){
+    return "OOPS";
+  }
+  if (Number(num) == 0){
+    return 0;
+  }
+  else if (Number(num) == 1 || Number(num) ==2){
+    return 1;
+  } else {
+    fib1 = 0;
+    fib2 = 1;
+    fibFinal = 0;
+    for(let i = 1; i < Number(num); i++){
+      fibFinal = fib1 + fib2;
+      fib1 = fib2;
+      fib2 = fibFinal;
+    }
+    return fibFinal;
+  }
+
 
 };
 
